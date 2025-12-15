ca7ff8ffa47da8b4bc4aa2f07a8d5d043d1af262
Jmail Filho <jamilf.alien@gmail.com>
2025-12-15 04:16:26 -0300

    add: página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Page } from "@/components/content/Page";
 import Image from "next/image";
 
 export default function Home() {
   return (
-      <></>
+      <>
+      <Page.Root>
+        <Page.Header>
+          <Page.Title content="Euaggelion" />
+          <Page.Description content="Semeando as boas novas da salvação" />
+        </Page.Header>
+      </Page.Root>
+      </>
   );
 }
