2ba35d4b1d64c1d1fc95c41998284c8852b18693
Jmail Filho <jamilf.alien@gmail.com>
2025-12-15 04:13:41 -0300

    add: layout padrão

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Menu } from "lucide-react";
+import { Toaster } from "@/components/ui/sonner";
+import { SiteHeader } from "@/components/layout/SiteHeader/";
+import { SiteFooter } from "@/components/layout/SiteFooter";
 
 export const metadata: Metadata = {
   title: "Euaggelion",
   description: "Semeando as boas novas da salvação",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="pt-br">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <main>        
+      <body className="bg-secondary">
+        <SiteHeader.Root>
+          <SiteHeader.Title text="Euaggelion" logo="/euaggelion-logo.svg" />
+          <SiteHeader.Navigation icon={Menu} />
+        </SiteHeader.Root>
+
+        <main>
           {children}
         </main>
+
+        <SiteFooter.Root>
+          <SiteFooter.Menu />
+          <SiteFooter.Copy copyright="CC0 1.0 Universal — Euaggelion" content="O projeto Euaggelion é uma iniciativa cristã independente cujo objetivo central é a divulgação gratuita de conteúdo cristão e teológico. Todo material disponibilizado por meio de nosso site está diponível sob uma licença de uso de domínio público." />
+        </SiteFooter.Root>
+        
+        <Toaster />
       </body>
     </html>
   );
