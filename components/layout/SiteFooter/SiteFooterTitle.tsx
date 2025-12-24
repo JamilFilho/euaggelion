fd915d50bc5159f0b5f13f540cd282bc45feab7f
Jmail Filho <jamilf.alien@gmail.com>
2025-12-24 18:33:59 -0300

    chore: updating the logo width and height declarations

--- components/layout/SiteFooter/SiteFooterTitle.tsx
+++ components/layout/SiteFooter/SiteFooterTitle.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 interface SiteFooterTitleProps {
@@ -12,7 +13,7 @@ export function SiteFooterTitle({ text, logo }: SiteFooterTitleProps) {
                 <h1>
                 {logo ? (
                     <>
-                    <img src={logo} alt={text} className="w-[10rem] grayscale dark:grayscale-0"/>
+                    <Image src={logo} alt={text} width={300} height={55} loading="lazy" className="w-[10rem]"/>
                     <span className="sr-only">{text}</span>
                     </>
                 ) : (
