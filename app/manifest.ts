9673107cdb32bebc6fafca082d846e12b33946a1
Jmail Filho <jamilf.alien@gmail.com>
2026-01-01 23:07:55 -0300

    feat: adding shortcuts

--- app/manifest.ts
+++ app/manifest.ts
@@ -24,6 +24,32 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ['education', 'religion', 'lifestyle'],
     lang: 'pt-BR',
     dir: 'ltr',
-    orientation: 'portrait-primary'
+    orientation: 'portrait-primary',
+    shortcuts: [
+      {
+        name: "Bíblia Sagrada",
+        short_name: "Bíblia",
+        description: "Leia as Sagradas Escrituras.",
+        url: "/biblia"
+      },
+      {
+        name: "Novas de Cada Manhã",
+        short_name: "Devocionais",
+        description: "Devocionais diários para edificar sua fé.",
+        url: "/s/cada-manha"
+      },
+      {
+        name: "TEOleigo",
+        short_name: "TEOleigo",
+        description: "Estudos e ensaios teológicos",
+        url: "/s/teoleigo"
+      },
+      {
+        name: "WikiGelion",
+        short_name: "Wiki",
+        description: "História do cristianismo, glossário teológico e artigos de referência.",
+        url: "/wiki"
+      }
+    ]
   }
 }
