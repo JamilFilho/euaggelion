b711daa248d7c9866e5a8e9832c097c03413a917
Jmail Filho <jamilf.alien@gmail.com>
2026-01-08 07:18:06 -0300

    fix: fixed link rendering in various scenarios.

--- components/content/Feed/FeedLink.tsx
+++ components/content/Feed/FeedLink.tsx
@@ -11,21 +11,33 @@ interface FeedLinkProps {
 }
 
 export default function FeedLink({ slug, category, articleCategory, trailSlug, isWiki = false, isCategoryPage = false }: FeedLinkProps) {
-    const href = isWiki || category === "wiki"
-        ? isCategoryPage
-            ? `/wiki/${articleCategory}/${slug}`
-            : `/wiki/${articleCategory}`
-        : category === "articles"
-            ? `/s/${slug}`
-            : category === "trilhas"
-            ? `/trilhas/${slug}`
-            : category === "steps"
-            ? `/trilhas/${trailSlug}/${slug}`
-            : `/${slug}`;
+    // Se é página de categorias wiki, o slug é a categoria
+    if (category === "wiki" && !isCategoryPage) {
+        return (
+            <Link href={`/wiki/${slug}`} className="flex flex-col gap-4">
+                <footer className="px-10 py-4 hover:pr-8 flex flex-row justify-between items-center border-t border-ring/20 md:border-b bg-black/10 hover:bg-black/20 transition-all ease-in-out text-sm text-foreground font-semibold">
+                    Ver artigos
+                    <ArrowRight />
+                </footer>
+            </Link>
+        );
+    }
+
+    const href = isWiki
+        ? `/wiki/${articleCategory}/${slug}`
+        : isCategoryPage || category === "wiki"
+            ? `/wiki/${articleCategory}`
+            : category === "articles"
+                ? `/s/${slug}`
+                : category === "trilhas"
+                ? `/trilhas/${slug}`
+                : category === "steps"
+                ? `/trilhas/${trailSlug}/${slug}`
+                : `/${slug}`;
 
     const content = isWiki
         ? category === "wiki"
-            ? "Ver artigos"
+            ? "Continuar lendo"
             : "Continuar lendo"
         : category === "articles"
             ? "Ver artigos"
