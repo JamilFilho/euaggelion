26638a3f395049d6b32bf65d70ac4f04568ec2d6
Jmail Filho <jamilf.alien@gmail.com>
2026-01-10 22:52:33 -0300

    chore: commentaries details

---
+++ lib/commentaries.tsx
@@ -0,0 +1,11 @@
+export interface TrailMeta {
+  name: string;
+  description?: string;
+}
+
+export const COMMENTARIES: Record<string, TrailMeta> = {
+    "joao-calvino": {
+        name: "João Calvino",
+        description: "Comentários bíblicos publicados por João Calvino.",
+    },
+}
