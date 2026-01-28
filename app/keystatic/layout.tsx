b8654ae5c5fc77c12149b39d4ebb06ae2a0c2bae
Jamil Filho <jamilf.alien@gmail.com>
2026-01-28 09:15:17 -0300

    fix: Clean up layout.tsx by removing unused code
    Removed commented-out code that checks showAdminUI.

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -1,12 +1,7 @@
 import KeystaticApp from "./keystatic";
-import { showAdminUI } from '../../keystatic.config'
 import { notFound } from "next/navigation";
 
 export default function Layout() {
-  // if (showAdminUI === false) {
-  //   notFound()
-  // }
-
   return (
     <KeystaticApp />
   );
